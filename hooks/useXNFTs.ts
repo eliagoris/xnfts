cdbcb99140b700e6ae89777a9aab5acaa02d933b
ellagoris <eduardofidera@gmail.com>
2022-10-02 22:27:23 -0300

    feat: add fetch xnfts

--- hooks/useXNFTs.ts
+++ hooks/useXNFTs.ts
@@ -1,24 +1,32 @@
 import { useConnection } from "@solana/wallet-adapter-react"
 import { web3, AnchorProvider } from "@project-serum/anchor"
-import { useEffect } from "react"
-import { fetchInstalls } from "lib/xnfts"
+import { useCallback, useEffect } from "react"
+import { fetchInstalls, fetchXNFTs } from "lib/xnfts"
 
 const useXNFTs = () => {
   const { connection } = useConnection()
+
+  const fetchAll = useCallback(async () => {
+    /** Read-only wallet. */
+    const dummyWallet = web3.Keypair.generate()
+
+    // @ts-ignore
+    const provider = new AnchorProvider(connection, dummyWallet, {
+      commitment: "confirmed",
+    })
+
+    // const installs = await fetchInstalls(provider)
+    // console.log(installs)
+
+    console.log("fetching...")
+    const xnfts = await fetchXNFTs(provider)
+    console.log("finished")
+    console.log(xnfts)
+  }, [])
+
   useEffect(() => {
-    ;(async () => {
-      /** Read-only wallet. */
-      const dummyWallet = web3.Keypair.generate()
-
-      // @ts-ignore
-      const provider = new AnchorProvider(connection, dummyWallet, {
-        commitment: "confirmed",
-      })
-
-      const installs = await fetchInstalls(provider)
-      console.log(installs)
-    })()
-  })
+    fetchAll()
+  }, [fetchAll])
 }
 
 export default useXNFTs
--- lib/xnfts.ts
+++ lib/xnfts.ts
@@ -80,3 +80,73 @@ export async function fetchInstalls(
 
   return xnfts
 }
+
+/**
+ * Fetch installed xNFTs.
+ */
+export async function fetchXNFTs(
+  provider: Provider,
+  wallet?: web3.PublicKey
+): Promise<
+  Array<{ publicKey: web3.PublicKey; medtadata: any; metadataBlob: any }>
+> {
+  const client = xnftClient(provider)
+
+  //
+  // Fetch all xnfts installed by this user.
+  //
+  const filters = []
+
+  if (wallet) {
+    filters.push({
+      memcmp: {
+        offset: 8, // Discriminator
+        bytes: wallet.toString(),
+      },
+    })
+  }
+
+  const xnftInstalls = await client.account.xnft.all(filters)
+
+  //
+  // Get the metadata accounts for all xnfts.
+  //
+  const metadataPubkeys = xnftInstalls.map(
+    ({ account }) => account.masterMetadata
+  )
+  const xnftMetadata = (
+    await utils.rpc.getMultipleAccounts(provider.connection, metadataPubkeys)
+  ).map((t) => {
+    if (!t) {
+      return null
+    }
+    return metadata.decodeMetadata(t.account.data)
+  })
+
+  //
+  // Fetch the metadata uri blob.
+  //
+  const xnftMetadataBlob = await Promise.all(
+    xnftMetadata.map((m) => {
+      if (!m) {
+        return null
+      }
+      return fetch(externalResourceUri(m.data.uri)).then((r) => r.json())
+    })
+  )
+
+  //
+  // Combine it all into a single list.
+  //
+  const xnfts = [] as any
+  metadataPubkeys.forEach((metadataPublicKey, idx) => {
+    xnfts.push({
+      metadataPublicKey,
+      metadata: xnftMetadata[idx],
+      metadataBlob: xnftMetadataBlob[idx],
+      install: xnftInstalls[idx],
+    })
+  })
+
+  return xnfts
+}
